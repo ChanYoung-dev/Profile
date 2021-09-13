29b3d1bc95a2b6bf548e911ec5a57a44dacd688b
emrhssla <emrhssla@gmail.com>
2021-09-13 17:39:04 +0900

    Javascript : Handle scrolling when tapping on th navbar menu
    메뉴버튼을 눌렀을 시 스크롤 활성화와 contact 에서 네이버블로그와 blog버튼 추가

--- main.js
+++ main.js
@@ -13,3 +13,19 @@ document.addEventListener('scroll', ()=>{
     }
 
 });
+
+// Handle scrolling when tapping on th navbar menu
+
+const navbarMenu = document.querySelector('.navbar_menu');
+
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
+})
