2b59c3e511c1e8cd0f292d5b44dfc4ee97aa80ed
emrhssla <emrhssla@gmail.com>
2022-01-18 22:56:33 +0900

    Do this after exporting notion files
    1. index.html 에서 <embed> 에서 src수정
    2. python파일실행
    3. 해당 html에 <script src="embed-main.js" defer></script> 추가
    4. 해당 html에 embed-container 추가

---
+++ delpy.py
@@ -0,0 +1,25 @@
+import os
+
+script = '<script src="../embed-sub.js" defer></script>'
+
+
+def replace_line(file_name, line_num, text):
+    lines = open(file_name, 'r').readlines()
+    lines[line_num] = text
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+
+for f_name in os.listdir('hellodev d23d460f2e554944bf60448e47f982ef'):
+    if f_name.endswith('html'):
+        html_file = open('./hellodev d23d460f2e554944bf60448e47f982ef/' + f_name, 'r')
+        line = html_file.readline()
+        print(line)
+        if script in line:
+            continue
+        lines=list(line)
+        lines.insert(12, script)
+        lines = ''.join(lines)
+        replace_line('./hellodev d23d460f2e554944bf60448e47f982ef/' + f_name, 0, lines)
+        html_file.close()
---
+++ test.py
@@ -0,0 +1,26 @@
+import os
+
+script = '<script src="../embed-sub.js" defer></script>'
+
+
+def replace_line(file_name, line_num, text):
+    lines = open(file_name, 'r').readlines()
+    lines[line_num] = text
+    out = open(file_name, 'w')
+    out.writelines(lines)
+    out.close()
+
+
+for f_name in os.listdir('test'):
+    if f_name.endswith('html'):
+        html_file = open('./test/' + f_name, 'r')
+        line = html_file.readline()
+        print(line)
+        if script in line:
+            continue
+        lines=list(line)
+        lines.insert(12, script)
+        lines = ''.join(lines)
+        replace_line('./test/' + f_name, 0, lines)
+        html_file.close()
+
