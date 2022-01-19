a44550e9cc11bdaadd3134e4f1a8c61b27f01f7f
emrhssla <emrhssla@gmail.com>
2022-01-19 18:13:06 +0900

    Add test.py / html들 파일바꾸기
    test.py는 html파일에서의 css와 script를 추가해주고, 일부 css를 수정해준다.

--- test.py
+++ test.py
@@ -1,6 +1,35 @@
+import numbers
 import os
+import re
+
+index_num = 12  
+
+original_text = """
+a,
+a.visited {
+	color: inherit;
+	text-decoration: hi;
+}
+"""
+input_text = """
+a,
+a.visited {
+	color: inherit;
+	text-decoration: changed;
+}
+"""
 
 script = '<script src="../embed-sub.js" defer></script><link rel="stylesheet" href="notion-mainstyle.css" />'
+scripts = 'hi'
+
+
+def insert_script_to_firstLine(file_name, script_text):
+    with open(file_name, 'r') as html_file:
+        first_line = list(html_file.readline())
+        first_line.insert(index_num, script_text)
+        completed_line = ''.join(first_line)
+        replace_line(file_name, 0, completed_line)
+    
 
 
 def replace_line(file_name, line_num, text):
@@ -11,16 +40,26 @@ def replace_line(file_name, line_num, text):
     out.close()
 
 
+def replace_paragraph(file_name, original_text, input_text):
+    data = open(file_name, 'r').read()
+    updated_data = re.sub(original_text, input_text, data)
+    with open(file_name, 'w') as out:
+        out.write(updated_data)
+    
+
+
+def read_first_line(file_name):
+    with open(file_name) as html_file:
+            first_line = html_file.readline()
+            return first_line
+
 for f_name in os.listdir('test'):
     if f_name.endswith('html'):
-        html_file = open('./test/' + f_name, 'r')
-        line = html_file.readline()
-        print(line)
-        if script in line:
-            continue
-        lines=list(line)
-        lines.insert(12, script)
-        lines = ''.join(lines)
-        replace_line('./test/' + f_name, 0, lines)
-        html_file.close()
+        first_line = read_first_line('./test/' + f_name)
+        if scripts in first_line:
+            print(first_line+"에는 " + scripts + "가 있습니다")
+        else:
+            insert_script_to_firstLine('./test/' + f_name, scripts)
+        replace_paragraph('./test/' + f_name, original_text, input_text)
+        
 
