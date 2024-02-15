63873a0a02ad0a2a3ce8479b17187cb6ca9f186f
emrhssla <emrhssla@gmail.com>
2024-02-15 16:26:06 +0900

    직장경력추가

---
+++ main2.js
@@ -0,0 +1,20 @@
+var number1 = 3
+var number2 = 6
+var result = number1 + number2;
+
+
+
+function add(numA, numB){
+
+    var result3 = numA + numB;
+    console.log(result3);
+    return result3+3;
+
+}
+
+var result2;
+console.log(add(3,8));
+console.log(add(number1,))
+
+
+console.log(result3);
